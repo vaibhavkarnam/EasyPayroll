949679a35d57152c86ec87b5a2521910516632e4
vaibhav <karnam.v@husky.neu.edu>
2019-04-25 20:56:24 -0400

    Front end application for Payroll application to add and edit employees along with their payroll information

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/tk-husky/'));
+app.use(express.static(__dirname + '/dist/easypayroll/'));
 
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/tk-husky/index.html'));
+res.sendFile(path.join(__dirname+'/dist/easypayroll/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
