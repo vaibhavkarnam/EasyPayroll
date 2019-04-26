2e1b99cea70a0982e843a3946053270f84b0ad21
vaibhav <karnam.v@husky.neu.edu>
2019-04-26 03:10:58 -0400

    Tweaks and Improvements

--- src/app/employee-edit/employee-edit.component.ts
+++ src/app/employee-edit/employee-edit.component.ts
@@ -17,12 +17,15 @@ export class EmployeeEditComponent implements OnInit {
   employee_department: String;
   employee_designation: String;
   employee_salary: Number;
+  employee_federal_tax: Number;
+  employee_state_tax: Number;
   employee_healthPremium: Number;
   employee_lifePremium: Number;
   employee_retirementPlan: Number;
-  img: String;
+  img_path: String;
   employee_emailId: String;
   employee_phone: String;
+  alert: boolean;
 
   constructor(private router: Router,
               private activatedRoute: ActivatedRoute,
@@ -31,17 +34,27 @@ export class EmployeeEditComponent implements OnInit {
   }
 
   updateEmployee() {
-    console.log('inside updateemployee');
+    if (this.employee_name == "" || this.employee_designation == "" || this.employee_salary == null ||
+      this.employee_federal_tax == null) {
+      this.alert = true;
+      return;
+    } else {
+      this.alert = false;
+    }
+
     var toUpdateemployee = {
       employee_name: this.employee_name,
       employee_bio: this.employee_bio,
       employee_department: this.employee_department,
-      img_path: this.img,
+      img_path: this.img_path,
       employee_designation: this.employee_designation,
       employee_salary: this.employee_salary,
       employee_healthPremium: this.employee_healthPremium,
+      employee_federal_tax: this.employee_federal_tax,
+      employee_state_tax: this.employee_state_tax,
       employee_lifePremium: this.employee_lifePremium,
       employee_retirementPlan: this.employee_retirementPlan,
+      employee_emailId: this.employee_emailId,
       employee_phone: this.employee_phone,
     };
 
@@ -52,12 +65,21 @@ export class EmployeeEditComponent implements OnInit {
   }
 
   postEmployee() {
+    if (this.employee_name == "" || this.employee_designation == "" || this.employee_salary == null ||
+      this.employee_federal_tax == null || this.employee_emailId == "") {
+      this.alert = true;
+      return;
+    } else {
+      this.alert = false;
+    }
     var toCreateEmployee = {
       employee_name: this.employee_name,
       employee_bio: this.employee_bio,
       employee_department: this.employee_department,
-      img_path: this.img,
+      img_path: this.img_path,
       employee_salary: this.employee_salary,
+      employee_federal_tax: this.employee_federal_tax,
+      employee_state_tax: this.employee_state_tax,
       employee_healthPremium: this.employee_healthPremium,
       employee_lifePremium: this.employee_lifePremium,
       employee_retirementPlan: this.employee_retirementPlan,
@@ -88,15 +110,18 @@ export class EmployeeEditComponent implements OnInit {
 
       this.employeeService.findEmployee(this.employeeId)
         .then(employee => {
+          console.log(employee);
           this.employee_name = employee.employee_name;
           this.employee_bio = employee.employee_bio;
           this.employee_department = employee.employee_department;
           this.employee_designation = employee.employee_designation;
           this.employee_salary = employee.employee_salary;
+          this.employee_federal_tax = employee.employee_federal_tax;
+          this.employee_state_tax = employee.employee_state_tax;
           this.employee_healthPremium = employee.employee_healthPremium;
           this.employee_lifePremium = employee.employee_lifePremium;
           this.employee_retirementPlan = employee.employee_retirementPlan;
-          this.img = employee.img_path;
+          this.img_path = employee.img_path;
           this.employee_emailId = employee.employee_emailId;
           this.employee_phone = employee.employee_phone;
         });
